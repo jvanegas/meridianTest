554a376d44f67257703eafc658e733e215cca8e8
jvanegas206 <juancamilo.vs206@gmail.com>
2023-02-09 01:05:16 -0500

    create folder structure and configure development tools

---
+++ config/index.js
@@ -0,0 +1,32 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const environment = process.env.NODE_ENV ?? 'DEVELOPMENT'
+let config
+
+switch (environment) {
+  case 'TEST':
+    config = {
+      username: process.env.DB_USERNAME ?? 'postgres',
+      password: process.env.DB_PASSWORD ?? 'postgres',
+      database: `${process.env.DB_NAME}_test` ?? 'test',
+      port: process.env.DB_PORT ?? 5432,
+      host: process.env.DB_HOST ?? '127.0.0.1',
+      dialect: process.env.DB_VENDOR ?? 'postgres',
+      appPort: process.env.APP_PORT ?? 3000,
+    }
+    break
+  case 'DEVELOPMENT':
+    config = {
+      username: process.env.DB_USERNAME ?? 'postgres',
+      password: process.env.DB_PASSWORD ?? 'postgres',
+      database: process.env.DB_NAME ?? 'postgres',
+      port: process.env.DB_PORT ?? 5432,
+      host: process.env.DB_HOST ?? '127.0.0.1',
+      dialect: process.env.DB_VENDOR ?? 'postgres',
+      appPort: process.env.APP_PORT ?? 3000,
+    }
+    break
+}
+
+export default config
