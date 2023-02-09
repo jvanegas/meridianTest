f6892c57ef6392c19fe8ff9a66e38cdc29532ba6
jvanegas206 <juancamilo.vs206@gmail.com>
2023-02-09 13:51:59 -0500

    add endpointo to get user's cart items

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,7 +1,7 @@
-import { addItem } from '../services/cart.js'
+import { addItem, getUserCart as getCart } from '../services/cart.js'
 
 const addProduct = async ({ db }, request, reply) => {
-  const userId = request.query.user_id ?? 'aaa'
+  const userId = request.query.user_id ?? ''
   const data = request.body
   try {
     const newItem = await addItem(db.models.CartItem, userId, data)
@@ -11,4 +11,14 @@ const addProduct = async ({ db }, request, reply) => {
   }
 }
 
-export { addProduct }
+const getUserCart = async ({ db }, request, reply) => {
+  const userId = request.params.userId ?? ''
+  try {
+    const userCart = await getCart(db.models.CartItem, userId)
+    reply.status(201).send(userCart)
+  } catch (e) {
+    reply.status(500).send({ message: e.message })
+  }
+}
+
+export { addProduct, getUserCart }
--- routes/cart.js
+++ routes/cart.js
@@ -1,9 +1,13 @@
-import { addProduct } from '../controllers/cart.js'
+import { addProduct, getUserCart } from '../controllers/cart.js'
 
 async function routes(fastify) {
   fastify.post(
     '/',
     async (request, reply) => await addProduct(fastify, request, reply),
+  )
+  fastify.get(
+    '/user/:userId',
+    async (request, reply) => await getUserCart(fastify, request, reply),
   )
 }
 
